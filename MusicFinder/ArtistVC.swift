d884cc96627ffe216c44c2c8aee9eb68d99cfa81
TRAING Serey <t.sereypich@gmail.com>
2017-06-16 20:10:20 +0200

    add xib artistvc

---
+++ MusicFinder/ArtistVC.swift
@@ -0,0 +1,25 @@
+//
+//  ArtistVC.swift
+//  MusicFinder
+//
+//  Created by TRAING Serey on 16/06/2017.
+//  Copyright © 2017 TRAING Serey. All rights reserved.
+//
+
+import UIKit
+
+class ArtistVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
